2dc6d85fdc92d695ae68431e00f4620a915d13c3
xXromkaXx <weromafurnan@gmail.com>
2025-02-22 13:31:26 +0200

    BD

--- src/main/java/org/example/DatabaseConnection.java
+++ src/main/java/org/example/DatabaseConnection.java
@@ -4,9 +4,9 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class DatabaseConnection {
-    private static final String URL = "jdbc:postgresql://localhost:5432/User";
-    private static final String USER = "postgres";
-    private static final String PASSWORD = "admin";
+    private static final String URL = System.getenv("DATABASE_URL"); // Отримуємо URL з змінної середовища
+    private static final String USER = System.getenv("DB_USER"); // Отримуємо юзера
+    private static final String PASSWORD = System.getenv("DB_PASSWORD"); // Отримуємо пароль
 
     public static Connection getConnection() throws SQLException {
         return DriverManager.getConnection(URL, USER, PASSWORD);
